351e7adca866bacecc38ce2c027ade150b4728aa
Manoj <Manoj@Manoj>
2023-10-26 20:40:00 +0530

    commit and push the project

---
+++ src/test/java/Demo/Sample.java
@@ -0,0 +1,13 @@
+package Demo;
+
+public class Sample {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello");
+		System.out.println("Manoj");
+		System.out.println("Kumar");
+
+	}
+
+}
